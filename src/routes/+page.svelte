3c3f26af74874598c1592492e5cff7fb4d0bf091
Brendan Fattig <54815652+brendanfattig@users.noreply.github.com>
2023-10-26 14:40:11 -0500

    edit homepage, restore readme, and add pnpm-lock

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,2 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to Group 3's Project 3 for CSCE 315-903</h1>
+<p>This is a work in progress.</p>
