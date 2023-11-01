1c20d2fc6df1ca2ba2e04616cadf1af883898121
meghasubhash11 <meghasubhash11@gmail.com>
2023-10-31 22:06:11 -0500

    added a few extra things

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -31,3 +31,24 @@
   <p>Home of delicious warm cookies delivered to your doorstep!</p>
   <button class="order-button">Order Now</button>
 </div>
+
+<div class="extra text">
+  <p>Founded in 1999, Tiff's Treats is the nation's first warm cookie delivery company.</p>
+  <p>Our cookies are baked fresh and delivered to your doorstep, warm and gooey.</p>
+  <p>Explore our mouthwatering selection of chocolate chip, snickerdoodle, and more!</p>
+</div>
+
+<div class="nav bar">
+  <a href="/menu">View Our Menu</a>
+  <a href="/about">Learn About Us</a>
+</div>
+
+<div class="contact">
+  <p>Contact Us: Phone - 123-456-7890 | Email - info@tiffstreats.com</p>
+</div>
+
+<div class="socials">
+  <a href="https://www.facebook.com/tiffstreats">Follow us on Facebook</a>
+  <a href="https://twitter.com/tiffstreats">Follow us on Twitter</a>
+</div>
+
