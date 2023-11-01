792b699a665f72e2adf202809bc5b973ea743600
meghasubhash11 <meghasubhash11@gmail.com>
2023-10-31 20:20:46 -0500

    basic home page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,33 @@
-<h1>Welcome to Group 3's Project 3 for CSCE 315-903</h1>
-<p>This is a work in progress to be delivered later.</p>
+
+<style>
+  /* Add your CSS styles here */
+  .container {
+    text-align: center;
+    padding: 20px;
+  }
+
+  h1 {
+    font-size: 36px;
+    color: #ff6347; 
+  }
+
+  p {
+    font-size: 18px;
+    margin-bottom: 20px;
+  }
+
+  .order-button {
+    padding: 10px 20px;
+    font-size: 18px;
+    background-color: #ff6347;
+    color: #fff;
+    border: none;
+    cursor: pointer;
+  }
+</style>
+
+<div class="container">
+  <h1>Welcome to Tiff's Treats</h1>
+  <p>Home of delicious warm cookies delivered to your doorstep!</p>
+  <button class="order-button">Order Now</button>
+</div>
