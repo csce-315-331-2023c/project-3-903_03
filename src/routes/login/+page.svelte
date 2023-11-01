21d85ebd37ad0d7b155745c35c208edc478bc903
aaronweng1 <aaron.weng@live.com>
2023-10-31 21:49:46 -0500

    login page from aaron branch

---
+++ src/routes/login/+page.svelte
@@ -0,0 +1,18 @@
+<img src="https://consultancy.innotecuk.com/wp-content/uploads/2017/10/cookies-banner.jpg" style="width:100%" height="175">
+<div style="width: 100%; height: 100%; background: #D9D9D9">
+    <p style="font-size: 40px; text-align: center; margin-bottom: 0 auto" >Welcome to Tiff's Treats! <br></p>
+    <form style="text-align: center" action="/action_page.php">
+        <label for="uname">username:</label>
+        <input type="text" id="uname" name="username"><br><br>
+        <label for="pword">password:</label>
+        <input type="password" id="pword" name="password"><br><br>
+        <input type="submit" value="Login" style="margin-bottom: 20px; background:white"> 
+        <input type="submit" value="Create New Account" style="background:#63C5DA">
+    </form>
+</div>
+
+<nav>
+    <a href="/">home</a>
+    <a href="/login">login</a>
+    <!-- Work on other navigation screens-->
+</nav>
