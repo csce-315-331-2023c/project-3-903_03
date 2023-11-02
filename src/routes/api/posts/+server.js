28551cfeb06d234775625010db0d9bc03cb550d5
aaronweng1 <aaron.weng@live.com>
2023-11-01 19:17:53 -0500

    javascript and server

--- src/routes/api/posts/+server.js
+++ src/routes/api/posts/+server.js
@@ -1,33 +1,37 @@
-export const GET = async ({ request }) => {
-    const authHeader = request.headers.get('Authorization')
-    
-    if (authHeader !== 'customer') {
-        return new Response(JSON.stringify({message: 'Invalid credentials'}), { status: 401 })
-    }
-
-    const res = await fetch('https://dummyjson.com/posts')
-    const data = await res.json()
-
-    return new Response(JSON.stringify(data), { status: 200 })
-}
-
 const { Pool } = require('pg');
 
 const pool = new Pool({
-  user: 'your_db_user',
+  user: 'csce315_903_03user',
   host: 'your_db_host',
-  database: 'your_db_name',
-  password: 'your_db_password',
+  database: 'csce315_903_03db',
+  password: 'lunar little lively',
   port: 'your_db_port',
 });
 
-// Example query
-const getSomeData = async () => {
+const getUsername = async () => {
   const client = await pool.connect();
   try {
-    const result = await client.query('SELECT * FROM your_table');
+    const result = await client.query('SELECT * FROM ');
     return result.rows;
   } finally {
     client.release();
   }
 };
+
+export const POST = async ({ request }) => {
+  const requestBody = await request.json();
+  const { username, password } = requestBody;
+
+  // check credentials
+  const dbResults = await getUsername(username);
+
+  if (dbResults.length === 1 && dbResults[0].password === password) {
+    // successful login
+    return new Response(JSON.stringify({ message: 'Login successful' }), { status: 200 });
+  } else {
+    // invalid credentials
+    return new Response(JSON.stringify({ message: 'Invalid credentials' }), { status: 401 });
+  }
+};
+
+
--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,4 +1,28 @@
-<img src="https://consultancy.innotecuk.com/wp-content/uploads/2017/10/cookies-banner.jpg" style="width:100%" height="175">
+<head>
+<script>
+    let username = '';
+    let password = '';
+    
+    async function handleLogin() {
+    
+    const response = await fetch('/api/posts', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    
+      if (response.status === 200) {
+        print("success");
+      } else {
+        print("error");
+      }
+    }
+</script>
+</head>
+
+<img src="https://consultancy.innotecuk.com/wp-content/uploads/2017/10/cookies-banner.jpg" alt="jpg"style="width:100%" height="175">
 <div style="width: 100%; height: 100%; background: #D9D9D9">
     <p style="font-size: 40px; text-align: center; margin-bottom: 0 auto" >Welcome to Tiff's Treats! <br></p>
     <form style="text-align: center" action="/action_page.php">
@@ -6,7 +30,7 @@
         <input type="text" id="uname" name="username"><br><br>
         <label for="pword">password:</label>
         <input type="password" id="pword" name="password"><br><br>
-        <input type="submit" value="Login" style="margin-bottom: 20px; background:white"> 
+        <input type="submit" value="Login" style="margin-bottom: 20px; background:white" onclick="handleLogin()"> 
         <input type="submit" value="Create New Account" style="background:#63C5DA">
     </form>
 </div>
@@ -14,5 +38,6 @@
 <nav>
     <a href="/">home</a>
     <a href="/login">login</a>
-    <!-- Work on other navigation screens-->
+    <!-- Work on other navigation screens -->
 </nav>
+
