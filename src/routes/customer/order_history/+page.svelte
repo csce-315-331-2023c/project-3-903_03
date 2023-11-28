08fecdac4652be1e8369202a88f3ecc6a2a0b08c
Sophia Dronova <sophiadronova@tamu.edu>
2023-11-27 20:45:24 -0600

    customer can view order history

--- src/routes/customer/order_history/+page.svelte
+++ src/routes/customer/order_history/+page.svelte
@@ -1,21 +1,130 @@
 <script>
-    let name = 'John Smith'
+  import { onMount } from 'svelte';
+  import { ListGroup, ListGroupItem } from 'sveltestrap';
+  import { Table, Image } from 'sveltestrap';
+  import { get_image } from "$lib/item_images";
+  import {
+    Button,
+    Card,
+    CardBody,
+    CardFooter,
+    CardHeader,
+    CardSubtitle,
+    CardText,
+    CardTitle
+  } from 'sveltestrap';
+
+  let name = 'John Smith';
+  let id = 6;
+  let customer_orders = [];
+
+  onMount(() => { 
+    get_data();
+    return () => {}; 
+  });
+
+  async function get_customer_orders(id) {
+    const input = `/customer/order_history/get_customer_orders?id=${id}`;
+    const response = await fetch(input);
+    const data = await response.json();
+    if (data.success) {
+      return data.customer_orders;
+    } else {
+      console.log(data.error);
+    }
+  }
+
+  async function get_ordered_items(customer_order_id) {
+    const input = `/customer/order_history/get_ordered_items?customer_order_id=${customer_order_id}`;
+    const response = await fetch(input);
+    const data = await response.json();
+    if (data.success) {
+      return data.ordered_items;
+    } else {
+      console.log(data.error);
+    }
+  }
+
+  async function get_data() {
+    let orders = await get_customer_orders(id);
+    if (orders) {
+      for (let order of orders) {
+        let items = await get_ordered_items(order.customer_order_id);
+        if (items) {
+          let customer_order = {
+            customer_order_id : order.customer_order_id,
+            order_date : order.order_date,
+            order_time : order.order_time,
+            cost : order.cost,
+            items : items,
+          };
+          customer_orders = [...customer_orders, customer_order];
+        }
+      }
+    }
+  }
+
   </script>
   
+  <title>Customer Order History</title>
   
-  <title>Customer</title>
-  
-  <div>
-      <header >Customer, { name }</header>
-  </div>
+  <header>
+    Customer, { name }
+  </header>
+  <header>
+    Order History
+  </header>
+ 
+  <body>
+    <ListGroup>
+      {#each customer_orders as customer_order}
+        <ListGroupItem >
+          <Card>
+            <CardHeader>
+              <CardTitle style="font-size: 40px">
+                Order #{customer_order.customer_order_id} 
+                was placed on {customer_order.order_date} 
+                at {customer_order.order_time}
+                with total cost {customer_order.cost}
+              </CardTitle>
+            </CardHeader>
+            <CardBody>
+              <Table bordered style="font-size: 30px">
+                <thead>
+                  <tr>
+                    <th>#</th>
+                    <th>Image</th>
+                    <th>Name</th>
+                    <th>Qty</th>
+                  </tr>
+                </thead>
+                <tbody>
+                    {#each customer_order.items as item }
+                      <tr>
+                        <td>{item.menu_item_id}</td>
+                        <td> 
+                          <Image src = {get_image(item.menu_item_id)} alt = {item.name} width="64" height="64"/>
+                        </td>
+                        <td>{item.name}</td>
+                        <td>{item.amount}</td>  
+                      </tr>
+                    {/each} 
+                </tbody>
+               </Table>
+              </CardBody>
+            <CardFooter/>
+          </Card>
+        </ListGroupItem>
+      {/each}
+    </ListGroup>  
+  </body>
 
   <style>
     header {
-        text-align: start;
-        font-size: 25px;
-        padding: 20px;
+        text-align: center;
+        font-size: 50px;
+        padding: 5px;
     }
-
     body {
       background-color: #f5f5f5; 
     }
---
+++ src/routes/customer/order_history/get_customer_orders/+server.js
@@ -0,0 +1,20 @@
+import pool from "$lib/db/pg";
+import { json } from '@sveltejs/kit';
+
+export async function GET({url}) {
+    let connection = await pool.connect();
+    const id = url.searchParams.get('id');
+    let sql = 
+        `SELECT co.customer_order_id, co.cost, \
+         TO_CHAR(co.order_date, 'YYYY-MM-DD') AS order_date, \
+         co.order_time AS order_time \
+         FROM _customer_order AS co WHERE co.id = '${id}'; `
+    try {
+        let result = await connection.query(sql);
+        return json({success: true, customer_orders: result.rows});
+    } catch {
+        return json({success: false, error: 'Orders were not found'});
+    } finally {
+        connection.release();
+    }
+}
