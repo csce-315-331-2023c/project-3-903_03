f180189b9eb569193be1c430f4094678ce3af017
meghasubhash11 <meghasubhash11@gmail.com>
2023-12-04 17:35:36 -0600

    high contrast color scheme and edited about page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,19 +1,94 @@
-<h1>About Tiff's Treats</h1>
+<style>
+  h1 {
+    font-size: 36px;
+    color: #0074cc; /* Shade of blue for heading */
+    margin-bottom: 20px;
+  }
 
-<h2> Our Story </h2>
-<p>Today, Tiff's Treats is famous for bringing hot, fresh-from-the-oven cookies and brownies to hungry treat lovers everywhere, but it all started with $20, a cell phone, and a dream.</p>
+  h2 {
+    font-size: 28px;
+    color: #004080; /* Darker shade of blue for subheadings */
+    margin-top: 20px;
+    margin-bottom: 10px;
+  }
 
-<h3> 1999 </h3>
-<h4> Born in Austin </h4>
-<p> When Tiffany Taylor and Leon Chen were college sophomores at the University of Texas, she stood him up for a date and delivered hot-from-the-oven cookies to apologize. Leon convinced Tiff to turn her cookies into a business, and Tiff’s Treats was born. The company started as small as you can get, with the two of them taking orders on a cell phone and baking cookies in the oven at Leon’s apartment. </p>
+  h3 {
+    font-size: 24px;
+    color: #001f3f; /* Darkest shade of blue for section headings */
+    margin-top: 15px;
+    margin-bottom: 8px;
+  }
 
-<h3> 2000-2003 </h3>
-<h4> Our First Storefront </h4>
-<p> In 2000, Tiff’s Treats expanded into a real kitchen on the Drag across from UT, sharing a space with Spudnik, a potato restaurant. In 2003, we moved into our own stand-alone location—a converted 1940’s bungalow at MLK & Nueces that still serves as our Central Austin location servicing UT and downtown. </p>
+  h4 {
+    font-size: 20px;
+    color: #004080; /* Darker shade of blue for subsection headings */
+    margin-top: 12px;
+    margin-bottom: 5px;
+  }
 
-<h3> 2009-2012 </h3>
-<h4> Fresh Favorites </h4>
-<p> We’ve always tried to keep our menu small, to keep our dough fresh and ensure we never run out of your favorite flavors. Of course, we’ve got ice cream and brownies, as well as a few other delicious delights. </p>
+  p {
+    font-size: 18px;
+    line-height: 1.6;
+    margin-bottom: 20px;
+  }
 
-<h2> Tiff's Treat's Today </h2>
-<p> Today, Tiff and Leon are married. And the company has grown to 86 stores and 1700 employees (and we're growing all the time). But our core mission is still the same- make amazing cookies from high-quality ingredients and bake them when they're ordered, so they're delivered fresh and warm. </p>
+  /* Optional: Add more styling for specific elements or sections */
+  .story-section {
+    background-color: #f5f5f5; /* Light gray background for the story section */
+    padding: 20px;
+    border-radius: 10px;
+  }
+
+  .year-section {
+    padding: 20px;
+  }
+
+  .year-section h3 {
+    font-size: 24px;
+    color: #001f3f;
+    margin-top: 8px; /* Reduce top margin */
+    margin-bottom: 4px; /* Reduce bottom margin */
+  }
+
+  .year-section h4 {
+    font-size: 20px;
+    color: #004080;
+    margin-top: 5px; /* Reduce top margin */
+    margin-bottom: 2px; /* Reduce bottom margin */
+  }
+
+  .current-state-section {
+    background-color: #f0f8ff; /* AliceBlue background for the current state section */
+    padding: 20px;
+    border-radius: 10px;
+  }
+</style>
+
+<div class="story-section">
+  <h2>Our Story</h2>
+  <p>Today, Tiff's Treats is famous for bringing hot, fresh-from-the-oven cookies and brownies to hungry treat lovers everywhere, but it all started with $20, a cell phone, and a dream.</p>
+</div>
+
+
+<div class="year-section">
+  <h3>1999</h3>
+  <h4>Born in Austin</h4>
+  <p>When Tiffany Taylor and Leon Chen were college sophomores at the University of Texas, she stood him up for a date and delivered hot-from-the-oven cookies to apologize...</p>
+</div>
+
+<div class="year-section">
+  <h3>2000-2003</h3>
+  <h4>Our First Storefront</h4>
+  <p>In 2000, Tiff’s Treats expanded into a real kitchen on the Drag across from UT, sharing a space with Spudnik, a potato restaurant. In 2003, we moved into our own stand-alone location—a converted 1940’s bungalow at MLK & Nueces that still serves as our Central Austin location servicing UT and downtown.</p>
+</div>
+
+<div class="year-section">
+  <h3>2009-2012</h3>
+  <h4>Fresh Favorites</h4>
+  <p>We’ve always tried to keep our menu small, to keep our dough fresh and ensure we never run out of your favorite flavors. Of course, we’ve got ice cream and brownies, as well as a few other delicious delights.</p>
+</div>
+
+<div class="current-state-section">
+  <h2>Tiff's Treat's Today</h2>
+  <p>Today, Tiff and Leon are married. And the company has grown to 86 stores and 1700 employees (and we're growing all the time). But our core mission is still the same- make amazing cookies from high-quality ingredients and bake them when they're ordered, so they're delivered fresh and warm.</p>
+</div>
